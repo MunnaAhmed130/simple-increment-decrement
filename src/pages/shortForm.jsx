55c96e719be15b27ac479ec88aa0f1c94969da2b
Munna Ahmed <munnaahmed2025@gmail.com>
2022-12-24 16:34:30 +0600

    commit

--- src/pages/shortForm.jsx
+++ src/pages/shortForm.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import RippleButton from "../component/rippleButton";
 
 const shortForm = () => {
+    const [name, setName] = useState("");
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(name);
+    };
+
     return (
         <div>
-            <form action="">
-                <div>
-                    <label for="name">Name</label> <br />
-                    <input type="text" name="name" id="name" />
+            <form className="form" onSubmit={submit}>
+                <div className="input-container">
+                    <label htmlFor="name">Name</label> <br />
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        onBlur={(e) => setName(e.target.value)}
+                    />
                 </div>
-                <RippleButton>Submit</RippleButton>
+                <RippleButton type="submit">Submit</RippleButton>
             </form>
         </div>
     );
