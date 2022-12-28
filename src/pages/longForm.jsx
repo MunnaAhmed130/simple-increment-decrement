9d347a912b94f971414b7a76c6f5271eb14c264c
Munna Ahmed <munnaahmed2025@gmail.com>
2022-12-29 01:13:05 +0600

    commit

--- src/pages/longForm.jsx
+++ src/pages/longForm.jsx
@@ -1,26 +1,34 @@
 import React from "react";
+import RippleButton from "../component/rippleButton";
 
 const longForm = () => {
     return (
         <div className="form--container">
             <form className="form--long">
                 <div className="col">
-                    <label for="firstName">
+                    <label>
                         First Name <br />
                         <input type="text" name="firstName" id="firstName" />
                     </label>{" "}
                     <br />
-                    <label for="email">
+                    <label>
                         Email <br />
                         <input type="email" name="email" id="email" />
                     </label>{" "}
                     <br />
-                    <label htmlFor="edu">
+                    <label>
                         Education <br />
-                        <select name="" id="edu">
-                            <option value="ssc">SSC</option>
-                            <option value="hsc">HSC</option>
-                        </select>
+                        <div className="select__container">
+                            <select name="" id="edu-select">
+                                <option value="ssc">SSC</option>
+                                <option value="hsc">HSC</option>
+                            </select>
+                            <span class="focus"></span>
+                        </div>
+                    </label>
+                    <label>
+                        Feedback <br />
+                        <textarea></textarea>
                     </label>
                 </div>
                 <div className="col">
@@ -29,20 +37,29 @@ const longForm = () => {
                         <input type="text" name="lastName" id="lastName" />
                     </label>{" "}
                     <br />
-                    <label for="">Gender</label>
+                    <label>Gender</label>
                     <br />
-                    <label for="">
-                        <input type="checkbox" name="" id="" />
-                        Male
-                    </label>
-                    <label for="">
-                        <input type="checkbox" name="" id="" />
-                        Female
-                    </label>
-                    <label for="">
-                        <input type="checkbox" name="" id="" />
-                        Others
-                    </label>
+                    <div className="gender-options">
+                        <label className="checkmark__container">
+                            <input type="radio" name="gender" id="male" />
+                            <span className="checkmark"></span>
+                            Male
+                        </label>
+                        <label className="checkmark__container">
+                            <input type="radio" name="gender" id="female" />
+                            <span className="checkmark"></span>
+                            Female
+                        </label>
+                        <label className="checkmark__container">
+                            <input type="radio" name="gender" id="others" />
+                            <span className="checkmark"></span>
+                            Others
+                        </label>
+                    </div>
+                    <label for="">Number of PCs</label> <br />
+                    <RippleButton className="decrement"> - </RippleButton>
+                    <input type="number" name="" value="" />
+                    <RippleButton> + </RippleButton>
                 </div>
             </form>
         </div>
