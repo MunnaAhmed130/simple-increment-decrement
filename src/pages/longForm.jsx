c95bc56b4af1f53620b5aa6a49c1dc5a1c9dd3d9
Munna Ahmed <munnaahmed2025@gmail.com>
2023-01-03 01:56:52 +0600

    commit

--- src/pages/longForm.jsx
+++ src/pages/longForm.jsx
@@ -19,16 +19,102 @@ const longForm = () => {
     return (
         <div className="form--container">
             <form className="form--long">
-                <div className="col">
+                <div className="row">
                     <label>
                         First Name <br />
                         <input type="text" name="firstName" id="firstName" />
-                    </label>{" "}
+                    </label>
                     <br />
+                    <label for="lastName">
+                        Last Name <br />
+                        <input type="text" name="lastName" id="lastName" />
+                    </label>
+                </div>
+                {/* <br /> */}
+                <div className="row">
                     <label>
                         Email <br />
                         <input type="email" name="email" id="email" />
                     </label>{" "}
+                    <br />
+                    <div className="gender">
+                        <label>Gender</label>
+                        <br />
+                        <div className="gender-options">
+                            <label className="checkmark__container">
+                                <input type="radio" name="gender" id="male" />
+                                <span className="checkmark"></span>
+                                Male
+                            </label>
+                            <label className="checkmark__container">
+                                <input type="radio" name="gender" id="female" />
+                                <span className="checkmark"></span>
+                                Female
+                            </label>
+                            <label className="checkmark__container">
+                                <input type="radio" name="gender" id="others" />
+                                <span className="checkmark"></span>
+                                Others
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <label>
+                        Education <br />
+                        <div className="select__container">
+                            <select name="" id="edu-select">
+                                <option value="ssc">SSC</option>
+                                <option value="hsc">HSC</option>
+                            </select>
+                            <span class="focus"></span>
+                        </div>
+                    </label>
+                    <br />
+                    <label className="pc_number">
+                        Number of PCs
+                        <div className="counter">
+                            <RippleButton
+                                className="decrement"
+                                onClick={(e) => {
+                                    dispatch({ type: "DECREMENT" });
+                                    e.preventDefault();
+                                }}
+                            >
+                                -
+                            </RippleButton>
+                            <span className="pc-count">{state}</span>
+                            <RippleButton
+                                className="increment"
+                                onClick={(e) => {
+                                    dispatch({ type: "INCREMENT" });
+                                    e.preventDefault();
+                                }}
+                            >
+                                +
+                            </RippleButton>
+                        </div>
+                    </label>
+                </div>
+                <div className="row">
+                    <label className="feedback">
+                        Feedback <br />
+                        <textarea></textarea>
+                    </label>
+                </div>
+                <div className="row">
+                    <RippleButton className="submit">Submit</RippleButton>
+                </div>
+                {/* <div className="col"> */}
+                {/* <label>
+                        First Name <br />
+                        <input type="text" name="firstName" id="firstName" />
+                    </label>
+                    <br />
+                    <label>
+                        Email <br />
+                        <input type="email" name="email" id="email" />
+                    </label>
                     <br />
                     <label>
                         Education <br />
@@ -72,23 +158,6 @@ const longForm = () => {
                     </div>
                     <label for="">Number of PCs</label> <br />
                     <div className="counter">
-                        {/* <RippleButton
-                            className="decrement"
-                            onClick={() =>
-                                setCounter((prevState) => prevState - 1)
-                            }
-                        >
-                            -
-                        </RippleButton>
-                        <div>{state}</div>
-                        <RippleButton
-                            onClick={() =>
-                                setCounter((prevState) => prevState - 1)
-                            }
-                        >
-                            +
-                        </RippleButton> */}
-
                         <RippleButton
                             className="decrement"
                             onClick={(e) => {
@@ -109,7 +178,7 @@ const longForm = () => {
                             +
                         </RippleButton>
                     </div>
-                </div>
+                </div> */}
             </form>
         </div>
     );
